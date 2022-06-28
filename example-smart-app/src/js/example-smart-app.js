f4ed99aa2d81bd134cfed9e67bccbe05f0269082
szacks <108175674+szacks@users.noreply.github.com>
2022-06-28 22:25:59 +0300

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -19,8 +19,9 @@
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given[0].join(' ');
+            //fname = patient.name[0].given[0].join(' ');
             //lname = patient.name[0].family.join(' ');
+            fname = "test first name";
             lname = "test";
           }
 
